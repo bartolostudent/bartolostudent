87dd325bdec0766f3b1ddbeab6501dfbf4489d3c
david <davidblancozafra@gmail.com>
2019-11-03 21:36:37 +0100

    Test Service

--- src/main/java/es/mockito/learn/service/impl/UserRelatedServiceImpl.java
+++ src/main/java/es/mockito/learn/service/impl/UserRelatedServiceImpl.java
@@ -1,13 +1,19 @@
 package es.mockito.learn.service.impl;
 
 import es.mockito.learn.entity.User;
+import es.mockito.learn.repository.UserRelatedRepo;
 import es.mockito.learn.service.UserRelatedService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserRelatedServiceImpl implements UserRelatedService {
+
+    @Autowired
+    UserRelatedRepo userRelatedRepo;
+
     @Override
     public void setRelatedUsers(User user1, User user2) {
-
+        userRelatedRepo.setRelatedUsers(user1, user2);
     }
 }
