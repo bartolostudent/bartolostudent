e990c2df3f6d363dfa17ace0cf9e9ef209950106
david <davidblancozafra@gmail.com>
2019-11-03 21:36:15 +0100

    Test Service

--- src/main/java/es/mockito/learn/repository/impl/UserRelatedRepoImpl.java
+++ src/main/java/es/mockito/learn/repository/impl/UserRelatedRepoImpl.java
@@ -6,10 +6,17 @@ import es.mockito.learn.repository.UserRelatedRepo;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+
 @Repository
 public class UserRelatedRepoImpl implements UserRelatedRepo {
+
+    HashMap<User, User> userHashMap = new HashMap<>();
+
     @Override
     public void setRelatedUsers(User user1, User user2) {
+
+        userHashMap.put(user1, user2);
 
     }
 }
