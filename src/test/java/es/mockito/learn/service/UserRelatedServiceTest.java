1470da437e8699282b021013eb7476fc6992211b
david <davidblancozafra@gmail.com>
2019-11-03 21:35:38 +0100

    Test Service

---
+++ src/test/java/es/mockito/learn/service/UserRelatedServiceTest.java
@@ -0,0 +1,12 @@
+package es.mockito.learn.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserRelatedServiceTest {
+
+    @Test
+    void setRelatedUsers() {
+    }
+}
